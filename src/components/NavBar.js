cbe455747d4f5e26db9ab89f8a9fa28de3b8b820
Mariano Quetglas <mariano_quetglas79@hotmail.com>
2022-09-04 10:17:24 -0300

    primeros cambios

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+
+const NavBar = () => {
+    return(<div>
+        <button>Home</button>
+        <button>Carrito</button>
+        <button>Contacto</button>
+        <h1 >Bienvenidos al ecommerce</h1>
+        </div>
+    )
+}
+
+export default NavBar;
